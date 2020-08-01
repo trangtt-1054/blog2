684c15f53d61d6de818a615aea722292d945de35
Tran Thu Trang <tranthutrang@Trans-MacBook-Pro.local>
2020-08-01 17:51:25 +0700

    cute

--- src/components/TagList/index.js
+++ src/components/TagList/index.js
@@ -1,13 +1,16 @@
-import React from "react"
+import React, { useContext } from "react"
 import { Link } from "gatsby"
+// import { GlobalDispatchContext } from "../../context/GlobalContextProvider"
 
 const TagList = props => {
+  //const dispatch = useContext(GlobalDispatchContext)
   const { tags } = props
   return (
     <div>
       {tags.map((tag, i) => (
         <span key={i} style={{ background: "yellow", marginRight: 10 }}>
           <Link
+            // onClick={() => dispatch({ type: "PAGE_CHANGE", payload: 1 })}
             to={`/stories/tags/${tag.toLowerCase()}`}
             style={{ textDecoration: "none" }}
           >
