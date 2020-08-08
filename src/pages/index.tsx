bf44895a0179d1e5ba9102fa3997f66623fc5933
Tran Thu Trang <tranthutrang@Trans-MacBook-Pro.local>
2020-08-08 20:13:31 +0700

    small clean up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,14 @@
 import React, { useContext, useEffect } from "react"
 import Layout from "../components/Layout"
 import { Link } from "gatsby"
-import {
-  GlobalDispatchContext,
-  GlobalStateContext,
-} from "../context/GlobalContextProvider"
+import { GlobalDispatchContext } from "../context/GlobalContextProvider"
 
 const Index = (props: any) => {
   const { uri } = props
-  const state = useContext(GlobalStateContext)
+
   const dispatch = useContext(GlobalDispatchContext)
   useEffect(() => dispatch({ type: "SET_ACTIVE_TAB", payload: "tab-0" }), [])
-  console.log(state)
+
   return (
     <Layout location={uri}>
       Home
--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,16 +1,13 @@
 import React, { useContext, useEffect } from "react"
 import Layout from "../components/Layout"
-import {
-  GlobalDispatchContext,
-  GlobalStateContext,
-} from "../context/GlobalContextProvider"
+import { GlobalDispatchContext } from "../context/GlobalContextProvider"
 
 const Portfolio = (props: any) => {
   const { uri } = props
-  const state = useContext(GlobalStateContext)
+
   const dispatch = useContext(GlobalDispatchContext)
   useEffect(() => dispatch({ type: "SET_ACTIVE_TAB", payload: "tab-2" }), [])
-  console.log(state)
+
   return <Layout location={uri}>Portfolio</Layout>
 }
 
